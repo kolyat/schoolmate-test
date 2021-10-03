9e20b317204fbe40ceac6c47816ee16ab92702d5
Kolyat <kks.pub@gmail.com>
2021-10-04 00:48:47 +0300

    Add script for updating apps and models

---
+++ update_apps.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+REPO=https://raw.githubusercontent.com/kolyat/schoolmate/master
+
+for app in school account news timetable diary
+do
+  curl ${REPO}/${app}/apps.py --output ./${app}/apps.py
+  curl ${REPO}/${app}/models.py --output ./${app}/models.py
+#  git archive --remote=git://github.com/kolyat/schoolmate \
+#              master:schoolmate/${app} apps.py models.py | tar -C ./${app} -xv
+done
